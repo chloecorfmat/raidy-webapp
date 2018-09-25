4ce1f9f1404fb4febd809461b8e35dd0de151c99
Lucas Trebouet <luuka.tr@gmail.com>
2018-09-25 13:23:04 +0200

    Init repo with basic FOSUserBundle

---
+++ README.md
@@ -0,0 +1,4 @@
+into-the-woods-webapp
+=====================
+
+A Symfony project created on September 22, 2018, 10:11 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 25/09/18
+ * Time: 12:46
+ */
+
+namespace AppBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
