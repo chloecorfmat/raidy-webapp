758b4f730cda587c84a206ca8c2f18a0e4416ed1
Lucas Trebouet <luuka.tr@gmail.com>
2018-10-14 18:16:25 +0200

    #246 - Fix multiple forms on helper profil page

--- src/HelperBundle/Controller/DefaultController.php
+++ src/HelperBundle/Controller/DefaultController.php
@@ -16,6 +16,7 @@ use AppBundle\Entity\Helper;
 use AppBundle\Entity\Raid;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
@@ -23,6 +24,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormError;
 
 class DefaultController extends Controller
 {
@@ -68,28 +70,43 @@ class DefaultController extends Controller
 
         $user = $this->getUser();
 
-        $form = $this->createFormBuilder($user, array('validation_groups' => array('editProfile')))
-            ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
-            ->add('firstName', TextType::class, ['label' => 'Prénom'])
-            ->add('lastName', TextType::class, ['label' => 'Nom'])
-            ->add('phone', TelType::class, ['label' => 'Numéro de téléphone'])
-            ->add('email', EmailType::class, ['label' => 'Adresse e-mail'])
-            ->add('submit', SubmitType::class, ['label' => 'Modifier le profil'])
-            ->getForm();
+        $formFactory = $this->get('form.factory');
 
-        $editPasswordform = $this->createFormBuilder($user, array('validation_groups' => array('changePassword')))
-            ->add('plainPassword', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les mots de passe doivent être identiques.',
-                'options' => array('attr' => array('class' => 'password-field')),
-                'required' => true,
-                'first_options'  => array('label' => 'Mot de passe'),
-                'second_options' => array('label' => 'Répétez le mot de passe'),
-            ))
-            ->add('submit', SubmitType::class, ['label' => 'Modifier le mot de passe'])
-            ->getForm();
+        $form = $formFactory->createNamedBuilder(
+            'editProfile',
+            'Symfony\Component\Form\Extension\Core\Type\FormType',
+            $user,
+            array('validation_groups' => array('editProfile', 'Profile'))
+        )
+        ->add('username', TextType::class, ['label' => 'Nom d\'utilisateur'])
+        ->add('firstName', TextType::class, ['label' => 'Prénom'])
+        ->add('lastName', TextType::class, ['label' => 'Nom'])
+        ->add('phone', TelType::class, ['label' => 'Numéro de téléphone'])
+        ->add('email', EmailType::class, ['label' => 'Adresse e-mail'])
+        ->add('submit', SubmitType::class, ['label' => 'Modifier le profil'])
+        ->getForm();
+
+        $data = [];
+        $editPasswordform = $formFactory->createNamedBuilder(
+            'editPwd',
+            'Symfony\Component\Form\Extension\Core\Type\FormType',
+            $data,
+            array('validation_groups' => array('changePassword'))
+        )
+        ->add('oldPassword', PasswordType::class, ['label' => 'Ancien mot de passe'])
+        ->add('plainPassword', RepeatedType::class, array(
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les mots de passe doivent être identiques.',
+            'options' => array('attr' => array('class' => 'password-field')),
+            'required' => true,
+            'first_options'  => array('label' => 'Nouveau mot de passe'),
+            'second_options' => array('label' => 'Répétez le mot de passe'),
+        ))
+        ->add('submit', SubmitType::class, ['label' => 'Modifier le mot de passe'])
+        ->getForm();
 
         $form->handleRequest($request);
+        $editPasswordform->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userManager = $this->get('fos_user.user_manager');
@@ -97,8 +114,19 @@ class DefaultController extends Controller
         }
 
         if ($editPasswordform->isSubmitted() && $editPasswordform->isValid()) {
-            $userManager = $this->get('fos_user.user_manager');
-            $userManager->updateUser($user);
+            $formData = $editPasswordform->getData();
+            $user = $this->getUser();
+
+            $encoder = $this->get('security.password_encoder');
+            $isPasswordValid = $encoder->isPasswordValid($user, $formData['oldPassword']);
+
+            if (!$isPasswordValid) {
+                $editPasswordform->addError(new FormError('Identifiants invalides'));
+            } else {
+                $user->setPlainPassword($formData['plainPassword']);
+                $userManager = $this->get('fos_user.user_manager');
+                $userManager->updateUser($user);
+            }
         }
 
         return $this->render('HelperBundle:Profile:editProfile.html.twig', [
