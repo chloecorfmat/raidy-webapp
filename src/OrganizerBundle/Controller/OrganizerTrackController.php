8f871eadbd62d8ca7e9df740c179f498b36c3993
Lucas Trebouet <luuka.tr@gmail.com>
2018-10-19 13:10:22 +0200

    Fix delete Track AJAX API

--- src/OrganizerBundle/Controller/OrganizerTrackController.php
+++ src/OrganizerBundle/Controller/OrganizerTrackController.php
@@ -157,15 +157,10 @@ class OrganizerTrackController extends AjaxAPIController
             return parent::buildJSONStatus(Response::HTTP_BAD_REQUEST, "You are not allowed to access this raid");
         }
 
-        $data = $request->request->all();
-
-        $trackService = $this->container->get('TrackService');
-
         $trackManager = $em->getRepository('AppBundle:Track');
         $track = $trackManager->find($trackId);
 
         if ($track != null) {
-            $track = $trackService->updateTrackFromArray($track, $raidId, $data);
             $em->remove($track);
             $em->flush();
         } else {
