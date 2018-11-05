9909267d2d1346865b2fe92e8e638c7c8681b1fe
Lucas Trebouet <luuka.tr@gmail.com>
2018-11-05 12:17:52 +0100

    Start add collaborator

---
+++ src/AppBundle/Entity/Collaboration.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 05/11/18
+ * Time: 11:44
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="collaboration")
+ */
+class Collaboration
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Raid")
+     * @ORM\JoinColumn()
+     * @Assert\NotBlank()
+     */
+    protected $raid;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=45)
+     * @Assert\NotBlank()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=13)
+     * @Assert\NotBlank()
+     */
+    private $invitationId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $user;
+
+    /**
+     * Collaboration constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRaid()
+    {
+        return $this->raid;
+    }
+
+    /**
+     * @param mixed $raid
+     */
+    public function setRaid($raid)
+    {
+        $this->raid = $raid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInvitationId()
+    {
+        return $this->invitationId;
+    }
+
+    /**
+     * @param mixed $invitationId
+     */
+    public function setInvitationId($invitationId)
+    {
+        $this->invitationId = $invitationId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+}
---
+++ src/OrganizerBundle/Controller/CollaborationController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucas
+ * Date: 05/11/18
+ * Time: 11:58
+ */
+
+namespace OrganizerBundle\Controller;
+
+class CollaborationController
+{
+
+    /*
+     *
+     *  /organizer/collaborator
+     *  /organizer/collaborator/add
+     *  /organizer/collaborator/delete
+     *
+     *  /collaborator/invite/{invitationId}
+     *  /collaborator/invite/success/{invitationId}
+     *  /collaborator/join/{invitationId}
+     *  /collaborator/register/{invitationId}
+     *
+     * */
+}
