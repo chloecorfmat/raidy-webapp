dedcd889f96d2be75202b0bb1e8fefb6eee1ddce
Chloé Corfmat <chloe.corfmat@capgemini.com>
2019-01-26 21:18:47 +0100

    Move gulpfile js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
@@ -9,21 +8,22 @@ const esLint = require('gulp-eslint');
 const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const merge = require('merge-stream');
+const uglify = require('gulp-uglify-es').default;
 
 
 // Linter.
 gulp.task('lint', function() {
-    gulp.src('./js/*.js')
+    gulp.src('./web/assets/js/*.js')
         //.pipe(esLint())
         .pipe(esLint.format());
         //.pipe(esLint.failAfterError());
 
-      gulp.src('./js/editor/*.js')
+      gulp.src('./web/assets/js/editor/*.js')
        // .pipe(esLint())
         .pipe(esLint.format());
       //.pipe(esLint.failAfterError());
 
-    gulp.src(['./scss/**/*.scss', '!./scss/lib/*.scss'])
+    gulp.src(['./web/assets/scss/**/*.scss', '!./web/assets/scss/lib/*.scss'])
         .pipe(sassLint())
         .pipe(sassLint.format())
         .pipe(sassLint.failOnError());
@@ -32,40 +32,39 @@ gulp.task('lint', function() {
 
 gulp.task('styles', function() {
     // place code for your default task here
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src('./web/assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false,
         }))
-        .pipe(gulp.dest('./css'))
         .pipe(rename('../dist/css/styles.css'))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./web/assets/css'))
         // Comment the line below to have unminify files.
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename('css/styles.min.css'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./web/assets/dist'));
 });
 
 gulp.task('scripts', function() {
-    var core = gulp.src(['./js/*.js', './js/editor/*.js', './js/race/*.js'])
+    var core = gulp.src(['./web/assets/js/*.js', './web/assets/js/editor/*.js', './web/assets/js/race/*.js'])
         .pipe(concat('../dist/js/scripts.js'))
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest('./web/assets/js'))
         // Comment the line below to have unminify files.
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(rename('js/scripts.min.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./web/assets/dist'));
 
-    var lib = gulp.src(['./js/lib/*.js'])
+    var lib = gulp.src(['./web/assets/js/lib/*.js'])
         .pipe(concat('../dist/js/lib.js'))
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest('./web/assets/js'))
         // Comment the line below to have unminify files.
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(rename('js/lib.min.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./web/assets/dist'));
 
     return lib;
 });
@@ -76,7 +75,7 @@ gulp.task('default', ['lint', 'styles', 'scripts'], function() {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./scss/**/*.scss', './js/**/*.js'], ['lint']);
-    gulp.watch('./scss/**/*.scss', ['styles']);
-    gulp.watch('./js/**/*.js', ['scripts']);
+    gulp.watch(['./web/assets/scss/**/*.scss', './web/assets/js/**/*.js'], ['lint']);
+    gulp.watch('./web/assets/scss/**/*.scss', ['styles']);
+    gulp.watch('./web/assets/js/**/*.js', ['scripts']);
 });
