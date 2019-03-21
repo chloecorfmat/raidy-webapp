7e7613463682b639b24b8c328e9e3d07c28ec84a
Chloé Corfmat <chloe.corfmat@capgemini.com>
2019-03-21 15:26:24 +0100

    Fix migrations

--- app/DoctrineMigrations/Version20181210155204.php
+++ app/DoctrineMigrations/Version20181210155204.php
@@ -16,6 +16,11 @@ class Version20181210155204 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->skipIf(
+            $schema->getTable('raid')->hasPrimaryKey('uniqid'),
+            'Skipping because `uniqid` primary key exists.'
+        );
+
         $this->addSql('ALTER TABLE raid CHANGE uniqid uniqid VARCHAR(255) NOT NULL');
         $this->addSql('CREATE UNIQUE INDEX raid_uniqid ON raid (uniqid)');
     }
