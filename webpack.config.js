5f6cfd931ebf1048b2887b05eee79dea31a74ab9
EPE <hello@epe.bzh>
2019-12-05 00:43:27 +0000

    Update things

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './web/assets/vue/app.js')
+    .addEntry('app', '../web/assets/vue/app.js')
     .addEntry('race', './web/assets/vue/raceEditor.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
